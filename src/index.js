f59d60cd63447246847817df9de79af29b6d72b4
Stella Nthenya <stellaalogi@gmail.com>
2023-04-25 23:50:40 +0300

    added some css on the nav bar

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// dom_elements
+const filter_poems_form = document.getElementById("filter_poems_form");
+const select_author = document.getElementById("authors");
+const filter_btn = document.getElementById("poem_name_btn");
+const poems_div = document.getElementsById("poems_list");
+
+// this is from https://github.com/public-apis/public-apis#books > https://github.com/thundercomb/poetrydb#
+const poems_api = "https://poetrydb.org/";
+
+
