11ebf19840a12e9e32f4510cf26476b9bfcd939e
Stella Nthenya <stellaalogi@gmail.com>
2023-04-26 00:40:17 +0300

    added some css on the form

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// dom_elements
+const filter_poems_form = document.getElementById("filter_poems_form");
+const select_author = document.getElementById("authors");
+const filter_btn = document.getElementById("poem_name_btn");
+const poems_div = document.getElementsById("poems_list");
+const nav_links = document.getElementsByClassName("nav-links");
+// this is from https://github.com/public-apis/public-apis#books > https://github.com/thundercomb/poetrydb#
+const poems_api = "https://poetrydb.org/";
