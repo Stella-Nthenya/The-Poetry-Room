db52bbb47ef293d6d68f1bdb59881de3ff322da4
Stella Nthenya <stellaalogi@gmail.com>
2023-04-26 03:19:43 +0300

    styling the page 5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 const filter_poems_form = document.getElementById("filter_poems_form");
 const select_author = document.getElementById("authors");
 const filter_btn = document.getElementById("poem_filter_btn");
-const poems_div = document.getElementsById("poems_list");
+const poems_div = document.getElementById("poems_list");
 const nav_links = document.getElementsByClassName("nav-links");
 // this is from https://github.com/public-apis/public-apis#books > https://github.com/thundercomb/poetrydb#
 const poems_api = "https://poetrydb.org/";
+
+// reading http request
